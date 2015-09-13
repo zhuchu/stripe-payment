c5cfb4a3ffed569188224c6faf336f35e40d5e95
Chu <chu@zhu.se>
2015-09-13 14:31:44 +0200

    imented first part of charges controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
 
   root 'pages#home'
 
+  # Support sripte payments through charges
+  resources :charges
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
